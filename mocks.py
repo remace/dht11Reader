53081de39b7ea66a0d1c1c2a50563eee0868daaf
Remi TAUVEL <tauvel.remi@gmail.com>
2019-12-27 01:27:54 +0100

    structure site

---
+++ mocks.py
@@ -0,0 +1,16 @@
+class Lecture():
+    DHT11 = {
+        'temperature' = 27.5,
+        'humidity' = 45
+    }
+    @classmethod
+    def all(cls):
+        pass
+
+    @classmethod
+    def temp(cls)
+        pass
+
+    @classmethod
+    def hum(cls):
+        pass

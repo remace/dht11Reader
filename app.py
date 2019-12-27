b6035f0f8984b0f3983dbd206c9779577a54bc23
Remi TAUVEL <tauvel.remi@gmail.com>
2019-12-27 02:38:10 +0100

    impression des mocks

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 from flask import Flask, render_template
-
+from mocks import Mocks
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('pages/home.html')
+
+@app.route('/stats')
+def stats():
+    posts = Mocks.all()
+    return render_template('pages/stats.html',posts = posts)
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('errors/404.html'), 404
 
 if __name__ == "__main__":
     app.run(debug = True, port=5000)
--- mocks.py
+++ mocks.py
@@ -1,16 +1,26 @@
-class Lecture():
+class Mocks():
     DHT11 = {
-        'temperature' = 27.5,
-        'humidity' = 45
+        'temperature' : 27.5,
+        'humidity' : 45
     }
+
+    POSTS = [
+        {'id': 1, 'temperature': 27.5, 'humidity': 45},
+        {'id': 2, 'temperature': 30, 'humidity': 30},
+        {'id': 3, 'temperature': 35, 'humidity': 25},
+        {'id': 4, 'temperature': 25, 'humidity': 80},
+        {'id': 5, 'temperature': 27.5, 'humidity': 45}
+    ]
+    
+
+    @classmethod
+    def DHT11_temp(cls):
+        pass
+
+    @classmethod
+    def DHT11_hum(cls):
+        pass
+
     @classmethod
     def all(cls):
-        pass
-
-    @classmethod
-    def temp(cls)
-        pass
-
-    @classmethod
-    def hum(cls):
-        pass
+        return cls.POSTS
